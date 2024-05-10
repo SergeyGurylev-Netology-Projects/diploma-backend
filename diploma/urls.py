2864a8123730250db16f9612ae1188870708b7e5
SergeyGurylev <ohg@mail.ru>
2024-05-10 23:15:11 +0300

    2024-05-10

--- diploma/urls.py
+++ diploma/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.generic import TemplateView
 
 from diploma import settings
 
@@ -14,7 +13,5 @@ urlpatterns = [
     path('download/<uuid>', issue_link_download),
     path('api/', include('my_cloud.urls')),
     path("", front, name="front"),
-    # re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name="index.html")),
     re_path(r'^(?:.*)/?$', front),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
